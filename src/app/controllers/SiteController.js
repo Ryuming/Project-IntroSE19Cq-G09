df2759a67f1caa3e4d02d13d354a201a02a90008
unknown <huuphuong130301@gmail.com>
2022-01-22 12:53:48 +0700

    complete project

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -24,15 +24,41 @@ class SiteController {
       .find({ username: { $regex: username, $options: "i" } })
       .lean()
       .exec(function (err, document) {
-        if (document.length === 0 || document[0].password !== password ) {
-        let a = 1;
-        res.render("login", {layout: "login", a })
+        if (document.length === 0 || document[0].password !== password) {
+          let a = 1;
+          res.render("login", { layout: "login", a });
         } else {
           res.redirect("/");
         }
-        
       });
-      return;
+    return;
+  }
+
+  search_category(req, res) {
+    const inp_search = req.query.inp_search;
+    data
+      .find({ title: { $regex: inp_search, $options: "i" } })
+      .lean()
+      .exec(function (err, document) {
+        if (inp_search == "") {
+          res.render("home", { err: "chua nhap du lieu" });
+        } else {
+          res.render("search", { document });
+        }
+      });
+  }
+  search_category_2(req, res) {
+    const inp_search = req.params.category;
+    data
+      .find({ category: { $regex: inp_search, $options: "i" } })
+      .lean()
+      .exec(function (err, document) {
+        if (inp_search == "") {
+          res.render("home", { err: "chua nhap du lieu" });
+        } else {
+          res.render("search", { document });
+        }
+      });
   }
 
   //[GET] /search
@@ -42,7 +68,34 @@ class SiteController {
       .find({ title: { $regex: inp_search, $options: "i" } })
       .lean()
       .exec(function (err, document) {
-        res.render("search", { document });
+        if (inp_search == "") {
+          res.render("home", { err: "chua nhap du lieu" });
+        } else {
+          res.render("search", { document, inp_search });
+        }
+      });
+  }
+  sort_Like(req, res) {
+    const inp_search = req.params.category;
+    data
+      .find({ title: { $regex: inp_search, $options: "i" } })
+      .lean()
+      .exec(function (err, document) {
+        function compareFirstNames( a, b ) {
+          if ( parseInt(a.reply) > parseInt(b.reply) ){
+            return -1;
+          }
+          if (parseInt(a.reply) < parseInt(b.reply) ){
+            return 1;
+          }
+          return 0;
+        }
+        document.sort(compareFirstNames);
+        if (inp_search == "") {
+          res.render("home", { err: "chua nhap du lieu" });
+        } else {
+          res.render("search", { document , inp_search});
+        }
       });
   }
 
--- src/routes/site.js
+++ src/routes/site.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const siteController = require('../app/controllers/SiteController');
 
 // newsController.index
+router.get('/search_category/:category', siteController.search_category_2);
+router.get('/sort_Like/:category', siteController.sort_Like)
 router.post('/checklogin', siteController.checklogin);
 router.get('/login', siteController.login);
 router.get('/search', siteController.search)
 router.get('/', siteController.index);
+router.get('/search_category', siteController.search_category);
 
 
 module.exports = router;
